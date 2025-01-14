05a5c2cf8d462c425da3becf09014e08d09f52f9
Daniel Caluff <dcaluff@gmail.com>
2025-01-14 15:16:28 -0500

    fixed error about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,9 @@ export default function Home() {
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Daniel Chaviano</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I capture the beauty of life through photography. Specializing in [specific
-        photography genre], I tell stories with every shot. Whether it's a candid smile or a breathtaking landscape, my
-        goal is to create images that inspire. Thank you for visiting my portfolio. Let&apos;s create something amazing
-        together.
+        I capture the beauty of life through photography. Specializing in [specific photography genre], I tell stories
+        with every shot. Whether it&apos;s a candid smile or a breathtaking landscape, my goal is to create images that
+        inspire. Thank you for visiting my portfolio. Let&apos;s create something amazing
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
         Feel free to adjust it as needed. If you need more help, just let me know!
